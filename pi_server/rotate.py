66fd0cfb25c6e69ec782c6f6da92fa32619b5d6d
Drew S. Ortega <DrewSOrtega@gmail.com>
2019-04-14 01:48:35 -0700

    changed rotate code to use new motor

--- pi_server/rotate.py
+++ pi_server/rotate.py
@@ -1,92 +1,21 @@
 import RPi.GPIO as GPIO
-import time 
+import time
 import sys
 
-out1 = 13
-out2 = 11
-out3 = 15
-out4 = 12
-
-i=0
-positive=0
-negative=0
-y=0
-
-speed=0.007
+out_vdd = 12
+out_gnd = 11
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(out1,GPIO.OUT)
-GPIO.setup(out2,GPIO.OUT)
-GPIO.setup(out3,GPIO.OUT)
-GPIO.setup(out4,GPIO.OUT)
-
-
-
+GPIO.setup(out_vdd, GPIO.OUT)
+GPIO.setup(out_gnd, GPIO.OUT)
+GPIO.output(out_vdd, GPIO.LOW)
+GPIO.output(out_gnd, GPIO.LOW)
 try:
-   while(1):
-       for i in range(0,8):
-          if i==0:
-              GPIO.output(out1,GPIO.HIGH)
-              GPIO.output(out2,GPIO.LOW)
-              GPIO.output(out3,GPIO.LOW)
-              GPIO.output(out4,GPIO.LOW)
-              time.sleep(speed)
-              #time.sleep(1)
-          elif i==1:
-              GPIO.output(out1,GPIO.HIGH)
-              GPIO.output(out2,GPIO.HIGH)
-              GPIO.output(out3,GPIO.LOW)
-              GPIO.output(out4,GPIO.LOW)
-              time.sleep(speed)
-              #time.sleep(1)
-          elif i==2:  
-              GPIO.output(out1,GPIO.LOW)
-              GPIO.output(out2,GPIO.HIGH)
-              GPIO.output(out3,GPIO.LOW)
-              GPIO.output(out4,GPIO.LOW)
-              time.sleep(speed)
-              #time.sleep(1)
-          elif i==3:    
-              GPIO.output(out1,GPIO.LOW)
-              GPIO.output(out2,GPIO.HIGH)
-              GPIO.output(out3,GPIO.HIGH)
-              GPIO.output(out4,GPIO.LOW)
-              time.sleep(speed)
-              #time.sleep(1)
-          elif i==4:  
-              GPIO.output(out1,GPIO.LOW)
-              GPIO.output(out2,GPIO.LOW)
-              GPIO.output(out3,GPIO.HIGH)
-              GPIO.output(out4,GPIO.LOW)
-              time.sleep(speed)
-              #time.sleep(1)
-          elif i==5:
-              GPIO.output(out1,GPIO.LOW)
-              GPIO.output(out2,GPIO.LOW)
-              GPIO.output(out3,GPIO.HIGH)
-              GPIO.output(out4,GPIO.HIGH)
-              time.sleep(speed)
-              #time.sleep(1)
-          elif i==6:    
-              GPIO.output(out1,GPIO.LOW)
-              GPIO.output(out2,GPIO.LOW)
-              GPIO.output(out3,GPIO.LOW)
-              GPIO.output(out4,GPIO.HIGH)
-              time.sleep(speed)
-              #time.sleep(1)
-          elif i==7:    
-              GPIO.output(out1,GPIO.HIGH)
-              GPIO.output(out2,GPIO.LOW)
-              GPIO.output(out3,GPIO.LOW)
-              GPIO.output(out4,GPIO.HIGH)
-              time.sleep(speed)
-              #time.sleep(1)
-  
-  
-             
+    while(1):
+        GPIO.output(out_vdd, GPIO.HIGH)
+        GPIO.output(out_gnd, GPIO.LOW)
+
 except KeyboardInterrupt:
-    GPIO.output(out1,GPIO.LOW)
-    GPIO.output(out2,GPIO.LOW)
-    GPIO.output(out3,GPIO.LOW)
-    GPIO.output(out4,GPIO.LOW)
+    GPIO.output(out_vdd, GPIO.LOW)
+    GPIO.output(out_gnd, GPIO.LOW)
     GPIO.cleanup()
